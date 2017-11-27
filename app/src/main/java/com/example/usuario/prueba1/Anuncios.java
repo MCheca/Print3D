f9edaee894213c460c7f87133ac34f1754f94676
MCheca <marcoscheca1@hotmail.com>
2017-11-27 12:10:29 +0100

    Añadido presupuesto máximo

--- app/src/main/java/com/example/usuario/prueba1/Anuncios.java
+++ app/src/main/java/com/example/usuario/prueba1/Anuncios.java
@@ -21,18 +21,23 @@ public class Anuncios extends AppCompatActivity {
 
     // Array of strings for ListView Title
     String[] listviewTitle = new String[]{
-            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+            "Mano para Sara", "Yoda", "ListView Title 3", "ListView Title 4",
             "ListView Title 5", "ListView Title 6", "ListView Title 7", "ListView Title 8",
     };
 
 
     int[] listviewImage = new int[]{
-            R.drawable.img1, R.drawable.img1, R.drawable.img1, R.drawable.img1,
+            R.drawable.mano, R.drawable.yoda, R.drawable.img1, R.drawable.img1,
             R.drawable.img1, R.drawable.img1, R.drawable.img1, R.drawable.img1,
     };
 
     String[] listviewShortDescription = new String[]{
+            "Sara necesita una mano nueva", "Tengo tiempo libre y quiero un Yoda en 3D", "Android ListView Short Description", "Android ListView Short Description",
             "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+    };
+
+    String[] listviewPresupuesto = new String[]{
+            "5€", "5€", "Android ListView Short Description", "Android ListView Short Description",
             "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
     };
 
@@ -48,11 +53,12 @@ public class Anuncios extends AppCompatActivity {
             hm.put("listview_title", listviewTitle[i]);
             hm.put("listview_discription", listviewShortDescription[i]);
             hm.put("listview_image", Integer.toString(listviewImage[i]));
+            hm.put("listview_presupuesto", "Presupuesto máximo:\n"+listviewPresupuesto[i]);
             aList.add(hm);
         }
 
-        String[] from = {"listview_image", "listview_title", "listview_discription"};
-        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+        String[] from = {"listview_image", "listview_title", "listview_discription", "listview_presupuesto"};
+        int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description, R.id.listview_presupuesto};
 
         SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.listview_activity, from, to);
         ListView androidListView = (ListView) findViewById(R.id.list_view);
